e9a5181db0b1bdc5ba120b10628c74af518fd285
Parth Parth <34807532+thecodingaviator@users.noreply.github.com>
2022-03-13 17:03:36 -0400

    remove extra keys

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY || process.env.API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN || process.env.AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID || process.env.PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET || process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID || process.env.MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID || process.env.APP_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
