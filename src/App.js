2e6068277f4df884e025db52a0f21e41f9b3b991
Parth Parth <34807532+thecodingaviator@users.noreply.github.com>
2022-03-13 11:23:27 -0400

    firebase setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MainArea from './components/MainArea.js';
 
+import { Container } from 'react-bootstrap';
+
 function App() {
   return (
-    <div className="App">
+    <Container>
       <MainArea />
-    </div>
+    </Container>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKED,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+export const auth = app.auth();
+export default app;
