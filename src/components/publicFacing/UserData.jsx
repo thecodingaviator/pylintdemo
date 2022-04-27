bea1ef20fa374df2c3ddb9ca8dbe977582884d4a
Parth Parth <34807532+thecodingaviator@users.noreply.github.com>
2022-04-26 20:14:13 -0400

    make charts look better

--- src/components/publicFacing/UserData.jsx
+++ src/components/publicFacing/UserData.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Link, useSearchParams } from 'react-router-dom';
-
-import ScatterPlot from '../support/ScatterPlot';
+import LineGraph from '../support/LineGraph';
 
 export default function UserData() {
   const [searchParams] = useSearchParams();
 
   return (
     <Container>
-      <h1>User Data</h1>
-      <div>
-        <ScatterPlot id={searchParams.get('userID')} />
+      <h1 className="mt-5 mb-3">User Data</h1>
+      <div className="mb-5">
+        <LineGraph id={searchParams.get('userID')} />
       </div>
       <Link to="/">
-        <button className="btn btn-primary w-100" type="button">
+        <button className="btn btn-primary w-100 mb-5" type="button">
           Click here to go back
         </button>
       </Link>
---
+++ src/components/support/LineGraph.jsx
@@ -0,0 +1,83 @@
+/* eslint-disable react/prop-types */
+import React, { useEffect } from 'react';
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+import { useAuth } from '../../contexts/AuthContext';
+
+export default function LineGraph(props) {
+  const { getScores } = useAuth();
+  const { id } = props;
+  const [scoreData, setScoreData] = React.useState([]);
+  const [labels, setLabels] = React.useState([]);
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Tooltip,
+    Legend,
+  );
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+    },
+  };
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Your Scores',
+        data: scoreData,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.2,
+        pointRadius: 0,
+      },
+    ],
+  };
+
+  useEffect(() => {
+    getScores(id).then((res) => {
+      let scores = res.score;
+      scores = scores.split(',');
+      if (scores[0] === 'undefined') {
+        scores.shift();
+      }
+
+      const dataNew = [];
+
+      for (let i = 0; i < scores.length; i += 1) {
+        const currScore = parseInt(scores[i], 10);
+        dataNew.push(currScore || 0);
+      }
+
+      setScoreData(dataNew);
+
+      const labelsNew = [];
+      const labelsLength = scores.length < 20 ? scores.length : scores.length / 5 + 1;
+
+      for (let i = 0; i < labelsLength; i += 1) {
+        labelsNew.push(i);
+      }
+
+      setLabels(labelsNew);
+    });
+  }, []);
+
+  return <Line options={options} data={data} />;
+}
